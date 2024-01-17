403e62f81e4551bb868b3630c894a8acb986b1b5
Sith Traly <sithtraly147@gmail.com>
2024-01-17 13:19:40 +0700

    Connect to database using sequelize and sqlite3

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const sequelize = require('sequelize')
+const path = require('path')
 
 const port = 3000
 const app = express()
+
+const db = new sequelize.Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'db.sqlite') // './db.sqlite'
+})
+
+try {
+  db.authenticate().then(() => {
+    console.log('database connected successfully.')
+  })
+} catch(err) {
+  console.log('cannot connect to database: ')
+  console.log(err)
+}
 
 app.set('view engine', 'ejs')
 app.set(express.static('public'))
